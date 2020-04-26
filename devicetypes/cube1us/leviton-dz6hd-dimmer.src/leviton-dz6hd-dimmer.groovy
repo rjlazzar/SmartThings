fd51143e3eefbbe59bb07e999965bedd1ec778ac
rjlazzar <62191213+rjlazzar@users.noreply.github.com>
2020-04-26 10:11:17 -0500

    Update leviton-dz6hd-dimmer.groovy

--- devicetypes/cube1us/leviton-dz6hd-dimmer.src/leviton-dz6hd-dimmer.groovy
+++ devicetypes/cube1us/leviton-dz6hd-dimmer.src/leviton-dz6hd-dimmer.groovy
@@ -438,7 +438,7 @@ def getLoadTypeValue() {
 //	Helper method to translate the string values for locatorStatus 
 //	into the numerical value expected by the hardware.
 
-def getLocatorStatusValue(String value) {
+def getLocatorStatusValue() {
 	switch(locatorStatus) {
     	case "never":
         	return(0)
